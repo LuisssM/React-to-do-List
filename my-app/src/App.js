af5021251528b01e2d028830420dcb56a2545c52
LuisssM <luispsmiranda.96@gmail.com>
2023-12-06 14:02:41 +0000

    final

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+//Aqui chamamos o que queremos como primeira página - todowrapper
+function App() {
+  return (
+    <div className="App">
+      
+      <TodoWrapper  />  
+    </div>
+  );
+}
+
+export default App;
